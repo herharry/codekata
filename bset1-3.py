da0adfa00dba5ebdde637a7d880eb4726a2b676f
suriya <48148739+herharry@users.noreply.github.com>
2019-07-10 14:32:28 +0530

    Update bset1-3.py

--- bset1-3.py
+++ bset1-3.py
@@ -1,7 +1,11 @@
 a=input()
 b=list()
+c=list()
+c=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
 b=['a','e','i','o','u']
 if(a in b):
   print("Vowel")
-else:
+elsif(a in c):
   print("Consonant")
+else:
+  print("Invalid")
