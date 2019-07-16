bba0e8952b36a0f88301a715daa5b4abc460e027
suriya <48148739+herharry@users.noreply.github.com>
2019-07-16 19:07:59 +0530

    Update hs1-3.py

--- hs1-3.py
+++ hs1-3.py
@@ -2,10 +2,14 @@ a=list()
 b=input()
 a = list(map(int,input().split()))
 c=list()
-flag=1
+flag=0
 for i in range(len(a)):
     if(a[i]==i):
-        print(i)
-        flag=0
+        c.append(i)
+        flag=1
 if(flag):
+    c.sort()
+    for i in range(len(a)):
+      print(c[i])
+else:
     print(-1)
