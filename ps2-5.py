ae937a4eee4dd7b47f7cb503258ec2a6d3831693
suriya <48148739+herharry@users.noreply.github.com>
2019-07-23 12:33:21 +0530

    Update ps2-5.py

--- ps2-5.py
+++ ps2-5.py
@@ -1,5 +1,14 @@
+def binary(x):
+  bform=list()
+  while(x>0):
+    bform.append(int(x%2))
+    x=int(x/2)
+  bform.reverse()
+  return bform
+
 a=int(input())
+x=list()
 b=list(map(int,input().split()))
-b.sort(reverse=True)
-for i in b:
-  print(i)
+for i in range(len(b)):
+  x.append(binary(b[i]))
+print(x)
